ca92c96dbb0b8a9c8934500328d0dfbbc25c1353
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-06-04 13:40:59 +0200

    Birthdate validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  
+  :recoverable, :rememberable, :validatable
+
+  # Callbacks
+  after_create :create_profile
+
   # Validations
   validates :username, presence: true
 
   has_one :profile
+
+  # Instance methods
+
 end
--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,7 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  describe 'Validations'
+  let(:user) { build :user }
+  let(:profile) { build :profile}
 
-  describe 'Associations'
+  describe 'Validations' do
+    context 'date of birth' do
+      it 'is valid if date is present' do
+        profile.valid?
+        expect(profile.errors[:date_of_birth]).to be_blank
+      end
+
+      it 'is invalid if date is absent' do
+        profile.date_of_birth = ''
+        profile.save
+        expect(profile.errors[:date_of_birth]).to include("can't be blank")
+      end
+
+      it 'is valid when age is >= 13' do
+        profile.date_of_birth = Date.new(2001, 2, 3)
+        profile.valid?
+        expect(profile.errors[:date_of_birth]).to be_blank
+      end
+
+      it 'is not valid when age is < 13' do
+        profile.date_of_birth = Date.new(2014, 2, 3)
+        profile.valid?
+        expect(profile.errors[:date_of_birth])
+          .to include('You are ineligible to register for devmedium')
+      end
+
+      it "is not valid with a date in the future" do
+        profile.date_of_birth = Date.new(4515, 02, 28)
+        expect(profile).to_not be_valid
+      end
+    end
+
+    context 'gender' do
+      it 'is either male or female' do
+        expect(profile.gender).to eq('male').or(eq('female')).or(eq('anything'))
+      end
+    end
+
+  end
 end
+
+# t.string "first_name"
+# t.string "last_name"
+# t.string "gender" => male or female
+# t.date "date_of_birth"=> can't be afte minimum age for site registeration
+# t.text "about_me"
