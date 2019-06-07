5477c46dace47f8ebd48d56bc9304e6ffa17266d
Saheed <sdrfngg640@gmail.com>
2019-06-07 14:04:47 +0100

    Add more test to user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { build :user }
+  let(:user) { create :user }
+  let(:request) { create :request }
   
   describe 'Associations' do
     it 'has one profile' do
       assc = described_class.reflect_on_association(:profile)
       expect(assc.macro).to eq :has_one
+    end
+
+    it 'has many friend requests' do
+      assc = described_class.reflect_on_association(:requests)
+      expect(assc.macro).to eq :has_many
+    end
+
+    it 'has many pending_friends' do
+      assc = described_class.reflect_on_association(:pending_friends)
+      expect(assc.macro).to eq :has_many
+    end
+
+    it 'is expected to destroy dependent requests' do
+      request
+      expect { user.destroy }.to change { user.requests.count }.by(-1)
     end
   end
 
