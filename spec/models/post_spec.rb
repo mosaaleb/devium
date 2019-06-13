ea401be5d5633e1bd42a1e8db61646ed0661f089
Saheed <sdrfngg640@gmail.com>
2019-06-13 13:57:46 +0100

    Refactor post specs

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,62 +7,56 @@ RSpec.describe Post, type: :model do
   let(:comment) { create :comment }
 
   describe 'Validations' do
-    context 'content' do
-      it 'is invalid when content is missing' do
+    context 'when content is missing' do
+      it 'is invalid' do
         post.post_content = nil
         post.valid?
         expect(post.errors[:post_content]).to include("can't be blank")
       end
+    end
 
-      it 'is valid when content is present' do
+    context 'when content is present' do
+      it 'is valid' do
         post.valid?
         expect(post.errors[:post_content]).to be_blank
       end
+    end
 
-      it 'is invalid  with more than 400 characters' do
+    context 'when content is > 400 charancters' do
+      it 'is invalid' do
         post.post_content = 'a' * 401
         post.valid?
         expect(post.errors[:post_content].to_s)
         .to include("maximum is 400")
       end
-      
-      it 'is valid when content has <= 400 characters' do
+    end
+
+    context ' when content has <= 400 characters' do
+      it 'is valid' do
         post.post_content = 'a' * 400
         post.valid?
         expect(post.errors[:post_content]).to be_blank
       end
     end
   end
+  
+  describe 'Associations' do    
+    it 'has many likes' do
+      user2.liked(post2)
+      expect(post2.likes.count).to eq 1
+    end
     
-  describe 'Associations' do
-    context 'likes' do
-      it 'has many likes' do
-        user2.liked(post2)
-        expect(post2.likes.count).to eq 1
-      end
-      
-      it 'returns numbers of likes associated with a post' do
+    it 'has many comments' do
+      assc = described_class.reflect_on_association(:comments)
+      expect(assc.macro).to eq :has_many
+    end
+
+    context 'when user likes post' do
+      it 'returns numbers of likes associated with the post' do
         user2.liked(post2)
         expect(post2.likes_count).to eq 1
-      end
-
-      it 'is dependent on post deletion' do        
-        user2.liked(post2)
-        expect { post2.destroy }.to change { Like.count }.by(-1)
-      end
+      end      
     end
 
-    context 'comments' do
-      it 'has many comments' do
-        assc = described_class.reflect_on_association(:comments)
-        expect(assc.macro).to eq :has_many
-      end
-      
-      it 'is dependent on post deletion' do
-        post2.comments << comment
-        post2.destroy
-        expect(Comment.count).to be 0
-      end
-    end
   end
 end
