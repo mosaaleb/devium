01dc0875a7079052c0d4ac9d85cc52628175ada2
Saheed <sdrfngg640@gmail.com>
2019-06-13 13:58:29 +0100

    Refactor friendship specs

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,23 +1,22 @@
 class Friendship < ApplicationRecord
   # Callback
-  # after_create :create_inverse_relationship
-  # after_destroy :destroy_inverse_relationship
 
   # Associations
   belongs_to :user
   belongs_to :friend, class_name: 'User'
 
   # Validations
-  validates :user_id, uniqueness: { scope: :friend_id }
+  validates :user_id, uniqueness: { context: friend_id}
+  validate :users_are_not_already_friends
 
-  # Instance Methods
-  # def create_inverse_relationship
-  #   friend.friendships.create friend: user
-  # end
+  private
 
-  # def destroy_inverse_relationship
-  #   friendship = friend.friendships.find_by(friend: user)
-  #   friendship.destroy if friendship
-  # end
+  def users_are_not_already_friends
+    # combinations = ["user_id = #{user_id} AND friend_id = #{friend_id}",
+    # "user_id = #{friend_id} AND friend_id = #{user_id}"]
 
+    # if Friendship.where(combinations.join(' OR ')).exists?
+    #   self.errors.add(:user_id, 'already friends!')
+    # end
+  end 
 end
--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,34 +1,62 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
+  let(:friendship) { create :friendship }
   let(:user) { create :user }
+  let(:user2) { create :user }
   let(:friend) { create :user }
 
   describe 'Validation' do
-    context 'user friendships' do
-      it 'is invalid when friendship already exists' do
+    context 'when friendship already exists' do
+      it 'is invalid' do
         user.friendships.create friend: friend
         friendship = user.friendships.create friend: friend
         expect(friendship.errors[:user_id]).to include("has already been taken")
+      end
+
+      it 'is invalid to' do
+        user.friendships.create friend: friend
+        friendship = friend.inverse_friendships.create friend: user
+        expect(friendship.errors[:user_id]).to include("already friends!")
       end
     end
   end
 
   describe 'Associations' do
-    context 'inverse friendships' do
-      it 'returns all inverse_friendships when called upon inverse_relationships' do
+    context 'when called upon inverse_relationships' do
+      it 'returns all inverse_friendships' do
         user.friendships.create friend: friend
         expect(user.friendships.first.id).to eq(friend.inverse_friendships.first.id)
       end
+    end
 
-      it 'returns all friends when called on #friends' do
+    context 'when called on friends' do
+      it 'returns all friends' do
         user.friendships.create friend: friend
         expect(user.friends.first.id).to eq(friend.id)
       end
+    end
 
-      it 'returns all friends when called on #inverse_friends' do
+    context 'when called on inverse_friends' do
+      it 'returns all friends' do
         user.friendships.create friend: friend
         expect(friend.inverse_friends.first.id).to eq(user.id)
+      end
+    end
+
+    context 'when user calls all_friends' do
+      it 'returns friends and inverse_friends' do
+        user.friendships.create friend: friend
+        friend.friendships.create friend: user2
+        
+        expect(friend.all_friends).to include(user && user2)
+      end
+    end
+
+    context 'when user is destroyed' do
+      it 'is expected to destroy dependent friendships' do
+        friendship
+        expect { friendship.user.destroy }.to change { Friendship.count }.by(-1)
       end
     end
   end
