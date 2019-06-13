eedf08ff710e98f8fea9b8e43fe37c328d73f183
Saheed <sdrfngg640@gmail.com>
2019-06-13 13:58:46 +0100

    Refactor comment specs

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,47 +2,59 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   let(:user) { build :user }
+  let(:post) { create :post }
   let(:comment) { build :comment }
   let(:user2) { create :user }
   let(:comment2) { create :comment }
 
   describe 'Validations' do
-    context 'content' do
-      it 'is invalid if the content is absent' do
+    context 'when content is missing' do
+      it 'is invalid' do
         comment.comment_content = nil
         comment.valid?
         expect(comment.errors[:comment_content]).to include("can't be blank")
       end
-      
-      it 'is valid when content is present' do
+    end
+    
+    context 'when content is present' do
+      it 'is valid' do
         comment.valid?
         expect(comment.errors[:comment_content]).to be_blank
       end
-      
-      it 'is invalid when content has more than 200 characters' do
+    end
+
+    context 'when content has more than 200 characters' do
+      it 'is invalid' do
         comment.comment_content = 'a' * 201
         comment.valid?
         expect(comment.errors[:comment_content].to_s)
         .to include("maximum is 200")
       end
-      
-      it 'is valid when content has <= 200 characters' do
+    end
+    
+    context 'when content has <= 200 characters' do
+      it 'is valid' do
         comment.comment_content = 'a' * 200
         comment.valid?
         expect(comment.errors[:comment_content]).to be_blank
       end
-      
     end
   end
 
-
   describe 'Association' do
-    context 'Likes' do
+    context 'when user likes a comment' do
       it 'returns numbers of likes associated with a comment' do
         user2.liked(comment2)
         expect(comment2.likes_count).to eq 1
       end
     end
+    
+    context 'when post is deleted' do      
+      it 'is expected to destroy dependent comments' do
+        post.comments << comment
+        post.destroy
+        expect(Comment.count).to be 0
+      end
+    end
   end
-
 end
