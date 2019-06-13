94b6b64290e5cdc05827f86f3364374c38de3e3b
Saheed <sdrfngg640@gmail.com>
2019-06-13 13:58:02 +0100

    Refactor like specs

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,21 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
+  let(:user) { create :user }
+  let(:post) { create :post }
+  let(:comment) { create :comment }
+
   describe 'Associations' do
-    
-    context 'user' do
-      it 'belongs to user' do
-        assc = described_class.reflect_on_association(:user)
-        expect(assc.macro).to eq :belongs_to
-      end
+    it 'belongs to user' do
+      assc = described_class.reflect_on_association(:user)
+      expect(assc.macro).to eq :belongs_to
     end
       
-    context 'likable' do
-      it 'belongs to likeable' do
-        assc = described_class.reflect_on_association(:likable)
-        expect(assc.macro).to eq :belongs_to
-      end
+    it 'belongs to likeable' do
+      assc = described_class.reflect_on_association(:likable)
+      expect(assc.macro).to eq :belongs_to
     end
     
+    context 'when user likes comments' do
+      it 'increases number of comments likes by one'  do
+        user.liked_comments << comment
+        expect(user.liked_comments.count).to eq 1
+      end      
+    end
+
+    context 'when user likes posts' do
+      it 'increases number of posts likes by one'  do
+        user.liked_posts << post
+        expect(user.liked_posts.count).to eq 1
+      end
+    end
+
+    context 'when user is deleted' do
+      it 'is expected to destroy dependent likes' do
+        user.liked(post)
+        expect { user.destroy }.to change { Like.count }.by(-1)
+      end      
+    end
+
+    context 'when post is deleted' do
+      it 'is expected to destroy dependent likes' do        
+        user.liked(post)
+        expect { post.destroy }.to change { Like.count }.by(-1)
+      end      
+    end
+
   end
 end
