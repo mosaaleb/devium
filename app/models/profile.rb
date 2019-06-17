c2840b666b2b49231c1a61a67c3e34f2242c2dcc
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-06-17 11:21:19 +0200

    Make age_is_present_and_premitted private

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,16 +1,18 @@
 class Profile < ApplicationRecord
+  # Enums
+  enum gender: [:female, :male]
+ 
   # Validations
   validate :age_is_present_and_permitted
   validates :date_of_birth, :gender, presence: true
   validates :about_me, length: { maximum: 400 }
 
-  # Instance methods
+  # Private methods
+  private
+
   def age_is_present_and_permitted
     if date_of_birth.present? && Date.today.year - date_of_birth.year < 13
       errors.add(:date_of_birth, 'You are ineligible to register for devmedium')
     end
   end
-
-  #enums
-  enum gender: [:female, :male]
 end
