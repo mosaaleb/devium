a8a906a631059a5813bd6792813768bfe193f59f
Saheed <sdrfngg640@gmail.com>
2019-06-20 18:34:34 +0100

    Update routes for post create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-
-  resources :users, only: :none, path: '', param: :username do
-    resource :profile, path: '', only: [:show, :edit, :update]
-    resources :posts, path: '', only: [:show, :edit, :update, :destroy]
-  end
-
-  devise_for :users, controllers: {
+  devise_for :users, path: 'accounts', controllers: {
     registrations: 'users/registrations'
   }
-
+  
   root 'newsfeed#show'
+  
+  resources :users, only: :none, path: '', param: :username do
+    resource :profile, path: '', only: [:show, :edit, :update]
+    resources :posts, path: '', only: [:show, :edit, :create, :update, :destroy]
+  end
 end
