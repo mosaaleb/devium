834f662c3c25681daa692d8c85a6d52733bafbcd
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-06-22 13:17:08 +0200

    Add destroy and update request specs && actions in comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,8 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
 
   def create
-    @post = Post.find(params[:id])
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.new comment_params
 
     current_user.adds_comment(@comment)
@@ -12,20 +13,32 @@ class CommentsController < ApplicationController
     else
       render 'newsfeeds/show'
     end
-  
   end
 
-  def edit
-  end
+  def update
+    @comment = Comment.find(params[:id])
 
-  def udpate
+    if  current_user != @comment.user
+      flash[:error] = 'You are not authorized to edit this comment'
+      redirect_to user_post_path @comment.post.user
+    elsif @comment.update comment_params
+      flash[:success] = 'Comment updated!'
+      redirect_to root_path
+    else
+      flash[:error] = 'Comment cannot be updated!'
+    end
   end
 
   def destroy
-    @post = Post.find(params[:id])
-    @comment = @post.comments.find(params[:comment_id])
+    @comment = Comment.find(params[:id])
 
-    current_user.deletes_comment(@comment)
+    if current_user == @comment.user
+      @comment.destroy
+      redirect_to root_path
+    else
+      redirect_to user_post_path @comment.post.user
+      flash[:error] = 'You are not authorized to delete this comment'
+    end
   end
 
   private 
--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -2,24 +2,35 @@ require 'rails_helper'
 
 RSpec.describe "Comments", type: :request do
   let(:comment) { create :comment }
+  let(:user2) { create :user}
+  let(:user) { create :user}
 
-  describe "POST /comments/:comment_id/add_comment" do
-    before do
-      sign_in comment.user
+  describe "POST /posts/:post_id/comments" do
+    context 'when not logged in' do
+      it 'redirects to sign in page' do
+        post "/posts/#{comment.post.id}/comments", params: { comment: { comment_content: 'I am a comment' } }
+
+        expect(response).to redirect_to '/accounts/sign_in'
+      end
     end
+
     
     context 'when logged in and parameters are valid' do
+      before do
+        sign_in comment.user
+      end
+
       it 'redirects to same page whereas comment been added' do
         params = { comment: { comment_content: 'I am a comment' } }
 
-        post "/posts/#{comment.post.id}/add_comment", params: params
+        post "/posts/#{comment.post.id}/comments", params: params
         expect(response).to redirect_to '/'
       end
 
       it 'shows sucsess flash message' do
         params = { comment: { comment_content: 'I am a comment' } }
 
-        post "/posts/#{comment.post.id}/add_comment", params: params
+        post "/posts/#{comment.post.id}/comments", params: params
         expect(flash[:success]).to eq('Comment successfully added!')
       end
     end
@@ -35,18 +46,68 @@ RSpec.describe "Comments", type: :request do
 
   end
 
-  describe 'DELETE /comments/:comment_id/remove_comment' do
-    before do
-      sign_in comment.user
-    end
+  describe 'PUT /comments/:id' do
     
-    context 'when logged in' do
-      it 'redirects to same page whereas comment been deleted' do
+    context 'when not logged in' do
+      it 'redirects to sign in page' do
+        put "/comments/#{comment.id}"
 
-        delete "/posts/#{comment.post.id}/remove_comment/#{comment.id}"
+        expect(response).to redirect_to '/accounts/sign_in'
+      end
+    end
+
+    context 'when logged in' do
+      before do
+        sign_in comment.user
+      end
+
+      it 'redirects to same page whereas comment been updated and show flash message' do
+        put "/comments/#{comment.id}", params: { comment: { comment_content: 'I am the new comment' } }
+
         expect(response).to redirect_to '/'
+        expect(flash[:success]).to eq 'Comment updated!'
+      end
+
+      it 'render again if new comment is not valid' do
+        put "/comments/#{comment.id}", params: { comment: { comment_content: '' } } 
+
+        expect(flash[:error]).to eq 'Comment cannot be updated!'
+      end
+
+      it 'authorize user for comment update' do
+        sign_in user2
+
+        put "/comments/#{comment.id}", params: { comment: { comment_content: 'I am the new comment' } }
+        # expect(response).to redirect_to "/#{comment.post.user.username}/#{comment.post.id}"
+        expect(flash[:error]).to include('You are not authorized to edit this comment')
       end
     end
   end
 
+  describe 'DELETE /comments/:id' do
+    context 'when not logged in' do
+      it 'redirects to sign in page' do
+        delete "/comments/#{comment.id}"
+
+        expect(response).to redirect_to '/accounts/sign_in'
+      end
+    end
+    
+    context 'when logged in' do
+      it 'redirects to same page whereas comment been deleted' do
+        sign_in comment.user
+
+        delete "/comments/#{comment.id}"
+        expect(response).to redirect_to '/'
+      end
+
+      it 'redirects to post page if the user is not authorized' do
+        sign_in user2
+
+        delete "/comments/#{comment.id}"
+        # expect(response).to redirect_to "/#{comment.post.user.username}/#{comment.post.id}"
+        expect(flash[:error]).to include('You are not authorized to delete this comment')
+      end
+    end
+  end
 end
