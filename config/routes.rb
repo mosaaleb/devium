2ddd8ceccfa7ffb0faa463d795a189d23fd39b3b
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-06-22 13:15:13 +0200

    Update routes to user resources instead of memeber routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,19 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
   
-  
 
   root 'newsfeed#show'
   
-  resources :posts, only: :none do
-    member do
-      post 'add_comment', to: 'comments#create'
-      delete 'remove_comment/:comment_id', to: 'comments#destroy', as: :delete_comment
-    end
+  resources :posts, only: :none, shallow: true do
+    # member do
+    #   post 'add_comment', to: 'comments#create'
+    #   put 'update_comment/:comment_id', to: 'comments#destroy', as: :update_comment
+    #   delete 'remove_comment/:comment_id', to: 'comments#destroy', as: :delete_comment
+    # end
+    resources :comments, only: [:update, :destroy, :create]
   end
   
+
   resources :users, only: :none, path: '', param: :username do
     resource :profile, path: '', only: [:show, :edit, :update]
     resources :posts, path: '', only: [:show, :edit, :create, :update, :destroy] do
