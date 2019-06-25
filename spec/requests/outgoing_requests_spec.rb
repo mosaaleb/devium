e83b98863201d6a9dd5ac329749298115bede2c7
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-06-25 17:56:44 +0200

    Break down requests controller into incoming and outgoing requests controllers

--- spec/requests/outgoing_requests_spec.rb
+++ spec/requests/outgoing_requests_spec.rb
@@ -1,12 +1,33 @@
 require 'rails_helper'
 
-RSpec.describe "Requests", type: :request do
+RSpec.describe "OutgoingRequests", type: :request do
   let(:request1) { create :request }
   
-  describe "POST /:id/:send_request" do
+  describe "POST relationships/:id/:send_request" do
     context 'when not logged in' do
       it "redirects to sign_in page" do
-        post "/#{request1.receiver.id}/send_request"
+        post "/relationships/#{request1.receiver.id}/send_request"
+
+        expect(response).to redirect_to('/accounts/sign_in')
+      end
+    end
+
+    context 'when logged in' do
+      it "redirects back to same page" do
+        sign_in request1.sender
+
+        post "/relationships/#{request1.receiver.id}/send_request"
+
+        expect(response).to redirect_to('/')
+      end
+    end   
+  end
+
+  describe "DELETE relationships/:id/remove_request" do
+
+    context 'when not logged in' do
+      it "redirects to sign_in page" do
+        delete "/relationships/#{request1.receiver.id}/remove_request"
       
         expect(response).to redirect_to('/accounts/sign_in')
       end
@@ -16,19 +37,17 @@ RSpec.describe "Requests", type: :request do
       it "redirects back to same page" do
         sign_in request1.sender
 
-        request_params = { request: { sender_id: request1.sender.id, receiver_id: request1.receiver.id } }
-        post "/#{request1.receiver.id}/send_request"
+        delete "/relationships/#{request1.receiver.id}/remove_request"
         
         expect(response).to redirect_to('/')
       end
     end   
   end
 
-  describe "DELETE /:id/:remove_request" do
-
+  describe "GET relationships/:id/remove_request" do
     context 'when not logged in' do
       it "redirects to sign_in page" do
-        delete "/#{request1.receiver.id}/remove_request"
+        get "/relationships/#{request1.receiver.id}/sent_requests"
       
         expect(response).to redirect_to('/accounts/sign_in')
       end
@@ -38,27 +57,7 @@ RSpec.describe "Requests", type: :request do
       it "redirects back to same page" do
         sign_in request1.sender
 
-        delete "/#{request1.receiver.id}/remove_request"
-        
-        expect(response).to redirect_to('/')
-      end
-    end   
-  end
-
-  describe "GET /:id/:remove_request" do
-    context 'when not logged in' do
-      it "redirects to sign_in page" do
-        get "/#{request1.receiver.id}/requests"
-      
-        expect(response).to redirect_to('/accounts/sign_in')
-      end
-    end
-
-    context 'when logged in' do
-      it "redirects back to same page" do
-        sign_in request1.sender
-
-        get "/#{request1.receiver.id}/requests"
+        get "/relationships/#{request1.receiver.id}/sent_requests"
         
         expect(response).to have_http_status 200
       end
