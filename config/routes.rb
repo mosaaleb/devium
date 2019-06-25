ec73696f3bec3f329406a546fc9a6d78e4031bc6
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-06-25 18:00:30 +0200

    Add incoming request and friendships routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   devise_for :users, path: 'accounts', controllers: {
     registrations: 'users/registrations'
   }
@@ -15,16 +16,24 @@ Rails.application.routes.draw do
     post 'like', to: 'likes#create'
     delete 'dislike', to: 'likes#destroy'
   end
-
+  
   resources :posts, only: :none, shallow: true, concerns: :likable do
     resources :comments, only: [:update, :destroy, :create], concerns: :likable
   end
 
-  resources :users, only: :none, path: '' do
+  resources :users, only: :none, path: :relationships do
     member do
-      get 'requests', to: 'requests#index'
-      post 'send_request', to: 'requests#create'
-      delete 'remove_request', to: 'requests#destroy'
+      get 'received_requests', to: 'incoming_requests#index'
+      delete 'reject_request', to: 'incoming_requests#destroy'
+    end
+    member do
+      get 'sent_requests', to: 'outgoing_requests#index'
+      post 'send_request', to: 'outgoing_requests#create'
+      delete 'remove_request', to: 'outgoing_requests#destroy'
+    end
+    member do
+      post 'accept_request', to: 'friendships#create'
+      delete 'remove_friend', to: 'friendships#destroy'
     end
   end
 
