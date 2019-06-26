33218095a3a46c45a1e176cbd6efa94087775986
Saheed <sdrfngg640@gmail.com>
2019-06-26 20:04:16 +0100

    Add comments form and partial to post

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,9 @@
 module CommentsHelper
+  def like_button(comment)
+    unless current_user.liked?(comment)
+      button_to 'Like', comment_like_path(comment)
+    else
+      button_to 'Unlike', comment_dislike_path(comment), method: :delete
+    end
+  end
 end
