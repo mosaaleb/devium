5e53c307c78e6f4a83e86e0f82b8bceddb89a2b7
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-06-26 17:29:13 +0200

    Add instance post and posts to show newsfeed action

--- app/controllers/newsfeeds_controller.rb
+++ app/controllers/newsfeeds_controller.rb
@@ -1,4 +1,6 @@
 class NewsfeedsController < ApplicationController
   def show
+    @post = current_user.posts.new
+    @posts = current_user.timeline_posts
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users, path: 'accounts', controllers: {
     registrations: 'users/registrations'
   }
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
     delete 'dislike', to: 'likes#destroy'
   end
   
-  resources :posts, only: :none, shallow: true, concerns: :likable do
+  resources :posts, only: [:create, :edit, :update, :destroy], shallow: true, concerns: :likable do
     resources :comments, only: [:update, :destroy, :create], concerns: :likable
   end
 
@@ -41,7 +41,7 @@ Rails.application.routes.draw do
 
   resources :users, only: :none, path: '', param: :username do
     resource :profile, path: '', only: [:show, :edit, :update]
-    resources :posts, path: '', only: [:show, :edit, :create, :update, :destroy]
+    resources :posts, path: '', only: :show
   end
 end
 
