ad53be88aa7d83bdc8d7f847eaa7bf4910d000a0
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-06-27 18:12:09 +0200

    Add case for handling nil current_user

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,9 +1,12 @@
 module CommentsHelper
-  def like_button(comment)
-    unless current_user.liked?(comment)
-      button_to 'Like', comment_like_path(comment)
-    else
-      button_to 'Unlike', comment_dislike_path(comment), method: :delete
+  def like_comment_button(comment)
+    case
+      when current_user.nil?
+        link_to 'Like', new_user_session_path
+      when current_user.liked?(comment)
+        button_to 'Unlike', post_dislike_path(comment), method: :delete
+      else
+        button_to 'Like', post_like_path(comment)
     end
   end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,10 +1,13 @@
 module PostsHelper
   
-  def like_button(post)
-    unless current_user.liked?(post)
-      button_to 'Like', post_like_path(post)
-    else
-      button_to 'Unlike', post_dislike_path(post), method: :delete
+  def like_post_button(post)
+    case
+      when current_user.nil?
+        link_to 'Like', new_user_session_path
+      when current_user.liked?(post)
+        button_to 'Unlike', post_dislike_path(post), method: :delete
+      else
+        button_to 'Like', post_like_path(post)
     end
   end
 
