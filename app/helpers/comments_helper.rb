b65ddacbbc5fa4993de519c153ade2d9d9930b02
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-06-28 12:12:47 +0200

    Fix bug in like and dislike path

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -4,9 +4,9 @@ module CommentsHelper
       when current_user.nil?
         link_to 'Like', new_user_session_path
       when current_user.liked?(comment)
-        button_to 'Unlike', post_dislike_path(comment), method: :delete
+        button_to 'Unlike', comment_dislike_path(comment), method: :delete
       else
-        button_to 'Like', post_like_path(comment)
+        button_to 'Like', comment_like_path(comment)
     end
   end
 end
