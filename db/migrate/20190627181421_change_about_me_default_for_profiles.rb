deafa5a7756235113c10c256257f045f132d3c51
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-06-28 10:26:16 +0200

    Make change about me migration reversible

--- db/migrate/20190627181421_change_about_me_default_for_profiles.rb
+++ db/migrate/20190627181421_change_about_me_default_for_profiles.rb
@@ -1,8 +1,17 @@
 class ChangeAboutMeDefaultForProfiles < ActiveRecord::Migration[5.2]
   def change
     change_column_default :profiles, :about_me, from: nil, to: "Apparently, this user prefers to keep an air of mystery about them."
-    Profile.find_each do |profile|
-      profile.update about_me: "Apparently, this user prefers to keep an air of mystery about them."
+    
+    reversible do |dir|
+      Profile.find_each do |profile|
+        dir.up do
+          profile.update about_me: "Apparently, this user prefers to keep an air of mystery about them."
+        end
+        dir.down do
+          profile.update about_me: nil
+        end
+      end
     end
+
   end
 end
