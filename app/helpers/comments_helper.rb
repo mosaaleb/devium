8147038a0d4cb8e2f91d4977c754266223021d13
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-07-04 16:53:58 +0200

    Add comments styling

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -22,4 +22,10 @@ module CommentsHelper
       link_to 'Delete', comment_path(comment.id), method: :delete, class: 'delete-comment-button'
     end
   end
+
+  def show_dropdown_comment_button(comment)
+    if edit_button(comment) && delete_button(comment)
+      render 'posts/dropdown'
+    end
+  end
 end
