ff27e7ec69ee04901ec233bb14ed53cbdf119c8a
Saheed <sdrfngg640@gmail.com>
2019-07-05 22:43:42 +0100

    Adjust navbar styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def avatar(user, size)
     image_sizes = { x_small: 30, small: 40, medium: 50, large: 60, x_large: 70, profile: 300 }
-    email_digest = Digest::MD5::hexdigest(user.email)
+    email_digest = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "//www.gravatar.com/avatar/#{email_digest}?&s=#{image_sizes[size]}"
     gravatar_url
   end
