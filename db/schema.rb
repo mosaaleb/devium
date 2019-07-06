8517ddd2753eb5362998ac2cf658bb8f5bb32ed1
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-07-06 21:14:52 +0200

    Add counter cache migration for comments count in posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_02_153611) do
+ActiveRecord::Schema.define(version: 2019_07_06_191115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2019_07_02_153611) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "likes_count", default: 0, null: false
+    t.integer "comments_count", default: 0, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
