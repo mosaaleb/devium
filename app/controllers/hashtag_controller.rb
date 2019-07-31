23174365c79db2b545af96691589668bf572f79a
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-07-31 19:02:14 +0200

    refactor show action in hashtag controller

--- app/controllers/hashtag_controller.rb
+++ app/controllers/hashtag_controller.rb
@@ -1,6 +1,8 @@
 class HashtagController < ApplicationController
+
   def show
     @hashtag = params[:id]
-    @posts_containing_hashtag = Post.where("post_content like ?", "%##{@hashtag}%")
+    @posts_containing_hashtag = Post.search(term: "##{@hashtag}")
   end
+
 end
