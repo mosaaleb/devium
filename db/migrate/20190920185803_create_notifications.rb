b3cbd9ac0d7ae90d28bb3fb2f230d8ab5260ed19
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-09-20 23:47:19 +0200

    Generate notification migration

---
+++ db/migrate/20190920185803_create_notifications.rb
@@ -0,0 +1,13 @@
+class CreateNotifications < ActiveRecord::Migration[5.2]
+  def change
+    create_table :notifications do |t|
+      t.integer :recipient_id
+      t.integer :actor_id
+      t.datetime :read_at
+      t.string :action
+      t.references :notifiable, polymorphic: true
+
+      t.timestamps
+    end
+  end
+end
