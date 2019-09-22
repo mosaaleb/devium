e798bbffde0dba44a97f10684ecbfac0a2e7a40b
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-09-22 22:17:48 +0200

    Why:
    * create post subscribers from every comment on that post and add its unit tests
    
    This change addresses the need by:
    
    * creating notification for every subscriber

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,9 @@ class Post < ApplicationRecord
   # Associations
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :subscribers, -> { distinct }, through: :comments, source: :user
   has_many :likes, as: :likable, dependent: :destroy
-  has_many :notifiactions, as: :notifiable, dependent: :destroy
+  has_many :notifications, as: :notifiable, dependent: :destroy
   # Validations
   validates :post_content, presence: true, length: { maximum: 400 }
 
