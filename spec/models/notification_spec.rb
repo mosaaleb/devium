eee0f2dddf80ca881f6a6ce7ceaf224428bdd507
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-10-05 23:26:16 +0200

    Add validation specs to notification model

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -3,5 +3,25 @@
 require 'rails_helper'
 
 RSpec.describe Notification, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:notification) { build :notification }
+  describe 'validations' do
+    it 'is invalid when recipient is missing' do
+      notification.recipient = nil
+      notification.save
+      expect(notification.errors[:recipient]).to include 'must exist'
+    end
+    it 'is invalid when actor is missing' do
+      notification.actor = nil
+      notification.save
+      expect(notification.errors[:actor]).to include 'must exist'
+    end
+    it 'is invalid when notifiable is missing' do
+      notification.notifiable = nil
+      notification.save
+      expect(notification.errors[:notifiable]).to include 'must exist'
+    end
+    it 'is valid when parameters are present' do
+      expect(notification.errors).to be_blank
+    end
+  end
 end
