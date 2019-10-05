8dbc967081885a2f3a71dc5e34a98cfb7552754c
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-10-05 23:35:51 +0200

    add notification model test for action presence

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -5,20 +5,25 @@ require 'rails_helper'
 RSpec.describe Notification, type: :model do
   let(:notification) { build :notification }
   describe 'validations' do
-    it 'is invalid when recipient is missing' do
+    it 'is invalid without recipient' do
       notification.recipient = nil
       notification.save
       expect(notification.errors[:recipient]).to include 'must exist'
     end
-    it 'is invalid when actor is missing' do
+    it 'is invalid without actor' do
       notification.actor = nil
       notification.save
       expect(notification.errors[:actor]).to include 'must exist'
     end
-    it 'is invalid when notifiable is missing' do
+    it 'is invalid without notifiable object' do
       notification.notifiable = nil
       notification.save
       expect(notification.errors[:notifiable]).to include 'must exist'
+    end
+    it 'is invalid without action' do
+      notification.action = nil
+      notification.save
+      expect(notification.errors[:action]).to include 'can\'t be blank'
     end
     it 'is valid when parameters are present' do
       expect(notification.errors).to be_blank
--- test/factories/notifications.rb
+++ test/factories/notifications.rb
@@ -2,6 +2,7 @@
 
 FactoryBot.define do
   factory :notification do
+    action { 'commented' }
     association :recipient, factory: :user, strategy: :create
     association :actor, factory: :user, strategy: :create
     association :notifiable, factory: :comment
