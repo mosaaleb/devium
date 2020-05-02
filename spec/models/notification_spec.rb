41858c3fdf8934f8390866193259d390548b21e0
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-05-02 06:16:31 +0200

    fix failing tests

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -4,27 +4,32 @@ require 'rails_helper'
 
 RSpec.describe Notification, type: :model do
   let(:notification) { build :notification }
+
   describe 'validations' do
     it 'is invalid without recipient' do
       notification.recipient = nil
       notification.save
       expect(notification.errors[:recipient]).to include 'must exist'
     end
+
     it 'is invalid without actor' do
       notification.actor = nil
       notification.save
       expect(notification.errors[:actor]).to include 'must exist'
     end
+
     it 'is invalid without notifiable object' do
       notification.notifiable = nil
       notification.save
       expect(notification.errors[:notifiable]).to include 'must exist'
     end
-    it 'is invalid without action' do
-      notification.action = nil
+
+    it 'is invalid without notifier' do
+      notification.notifier = nil
       notification.save
-      expect(notification.errors[:action]).to include 'can\'t be blank'
+      expect(notification.errors[:notifier]).to include 'must exist'
     end
+
     it 'is valid when parameters are present' do
       expect(notification.errors).to be_blank
     end
--- test/factories/notifications.rb
+++ test/factories/notifications.rb
@@ -2,9 +2,9 @@
 
 FactoryBot.define do
   factory :notification do
-    action { 'commented' }
     association :recipient, factory: :user, strategy: :create
     association :actor, factory: :user, strategy: :create
     association :notifiable, factory: :comment
+    association :notifier, factory: :comment
   end
 end
