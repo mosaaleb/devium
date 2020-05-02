ad07f424fe9bf36aafdcab6c61f57068281e92c9
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-05-03 01:39:59 +0200

    add more tests to newsfeed request specs

--- spec/requests/newsfeeds_spec.rb
+++ spec/requests/newsfeeds_spec.rb
@@ -1,16 +1,37 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Newsfeeds", type: :request do
-  describe "GET /" do
-    let(:user)  { create :user }
-    
+RSpec.describe 'Newsfeeds', type: :request do
+  describe 'GET /' do
+    let(:user) { create :user }
+    let!(:user_post) { create(:post, user: user) }
+
     context 'when not logged in' do
-      it "redirects to sign_in page" do
+      it 'redirects to sign_in page' do
         get '/'
-        
-        expect(response.body).to include("login")
+
+        expect(response.body).to include('login')
       end
     end
-    
+
+    context 'when user is logged in' do
+      before do
+        sign_in user
+        get '/'
+      end
+
+      it 'rendered page displays user firstname' do
+        expect(response.body).to include(user.first_name.capitalize)
+      end
+
+      it 'rendered page displays a publish button' do
+        expect(response.body).to include('Publish')
+      end
+
+      it 'rendered page displays contains user own posts' do
+        expect(response.body).to include(user_post.post_content)
+      end
+    end
   end
 end
