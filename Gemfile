b15fe6a498f6b9d568ffb413281a0668144fbb36
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-05-05 23:13:15 +0200

    modify liking feature spec with js: true option
    add selenium webdriver

--- Gemfile
+++ Gemfile
@@ -50,6 +50,7 @@ group :test do
   gem 'faker'
   gem 'launchy'
   gem 'rspec-html-matchers'
+  gem 'selenium-webdriver'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/features/like_actions_spec.rb
+++ spec/features/like_actions_spec.rb
@@ -1,47 +1,51 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.feature "LikeActions", type: :feature do
+RSpec.describe 'LikeActions', type: :feature do
+  let(:current_user) { create :user }
   let(:comment) { create :comment }
 
   before do
-    feature_sign_in(comment.user.email, comment.user.password)
+    feature_sign_in(current_user.email, current_user.password)
     fill_in "What's happening?", with: comment.post.post_content
     click_on 'Publish'
-    fill_in 'Comment on this post.', with: 'First test comment'
+    click_on 'Add Comment'
+    fill_in 'comment[comment_content]', with: 'Just a comment!'
     click_on 'Comment'
   end
 
-  context "when it is a post" do
-    scenario 'likes the post' do
-      within ".post-likes" do
+  context 'when it is a post' do
+    it 'likes the post', js: true do
+      within '.post-likes' do
         click_link
       end
-      expect(page).to have_css(".badge", text: "1")
+      expect(page).to have_css('.badge', text: '1')
     end
 
-    scenario 'unlikes the post' do
-      within ".post-likes" do
+    it 'unlikes the post', js: true do
+      within '.post-likes' do
         click_link
         click_link
       end
-      expect(page).to have_css(".badge", text: "0")
+      expect(page).to have_css('.badge', text: '0')
     end
   end
 
-  context "when it is a comment" do
-    scenario 'likes the comment' do
-      within ".comment-likes" do
+  context 'when it is a comment' do
+    it 'likes the comment', js: true do
+      within '.comment-likes' do
         click_link
       end
-      expect(page).to have_content("1")
+      expect(page).to have_content('1')
     end
 
-    scenario 'unlikes the comment' do
-      within ".comment-likes" do
+    it 'unlikes the comment', js: true do
+      within '.comment-likes' do
         click_link
         click_link
       end
-      expect(page).to have_css(".badge", text: "0")
+      expect(page).to have_css('.badge', text: '0')
     end
   end
 end
