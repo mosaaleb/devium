c10c552bb28609cd570c2e2b2fdff19ed93998bb
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-05-08 04:05:14 +0200

    rewrite model specs with shouldamatchers

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,85 +3,44 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  let(:user) { build :user }
-  let(:post) { create :post }
-  let(:comment) { build :comment }
-  let(:user2) { create :user }
-  let(:comment2) { create :comment }
   let(:john) { create :user }
   let(:aida) { create :user }
   let(:followed_post) { create :post }
 
   describe 'Validations' do
-    context 'when content is missing' do
-      it 'is invalid' do
-        comment.comment_content = nil
-        comment.valid?
-        expect(comment.errors[:comment_content]).to include("can't be blank")
-      end
-    end
-
-    context 'when content is present' do
-      it 'is valid' do
-        comment.valid?
-        expect(comment.errors[:comment_content]).to be_blank
-      end
-    end
-
-    context 'when content has more than 200 characters' do
-      it 'is invalid' do
-        comment.comment_content = 'a' * 201
-        comment.valid?
-        expect(comment.errors[:comment_content].to_s)
-          .to include('maximum is 200')
-      end
-    end
-
-    context 'when content has <= 200 characters' do
-      it 'is valid' do
-        comment.comment_content = 'a' * 200
-        comment.valid?
-        expect(comment.errors[:comment_content]).to be_blank
-      end
-    end
+    it { is_expected.to validate_presence_of(:comment_content) }
+    it { is_expected.to validate_length_of(:comment_content).is_at_most(200) }
   end
 
   describe 'Association' do
-    context 'when user likes a comment' do
-      it 'returns numbers of likes associated with a comment' do
-        user2.like(comment2)
-        expect(comment2.likes_count).to eq 1
-      end
-    end
-
-    context 'when post is deleted' do
-      it 'is expected to destroy dependent comments' do
-        post.comments << comment
-        post.destroy
-        expect(described_class.count).to be 0
-      end
-    end
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:post) }
+    it { is_expected.to have_many(:likes) }
+    it { is_expected.to have_many(:mentions) }
+    it { is_expected.to have_many(:notifications) }
   end
 
-  context 'when created' do
-    before do
-      john.comments.create(comment_content: 'I am a comment',
-                           post_id: followed_post.id)
-    end
+  describe 'creating notifications' do
+    context 'when created' do
+      before do
+        john.comments.create(comment_content: 'Just a comment',
+                             post_id: followed_post.id)
+      end
 
-    it 'creates notification for commented post original author' do
-      expect(Notification.where(recipient: followed_post.user))
-        .not_to be_empty
-    end
+      it 'creates notification for commented post original author' do
+        expect(Notification.where(recipient: followed_post.user))
+          .not_to be_empty
+      end
 
-    it 'creates notification for commented post subscribers' do
-      aida.comments.create(comment_content: 'I am a comment',
-                           post_id: followed_post.id)
-      expect(Notification.count).to be 3
-    end
+      it 'creates notification for commented post subscribers' do
+        aida.comments.create(comment_content: 'I am a comment',
+                             post_id: followed_post.id)
+        expect(Notification.count).to be 3
+      end
 
-    it 'does not create a notification for comment author' do
-      expect(Notification.where(recipient: john)).to be_empty
+      it 'does not create a notification for comment author' do
+        expect(Notification.where(recipient: john)).to be_empty
+      end
     end
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,73 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  let(:post) { build :post }
-  let(:user2) { create :user }
-  let(:post2) { create :post }
-  let(:comment) { create :comment }
-
   describe 'Validations' do
-    context 'when content is missing' do
-      it 'is invalid' do
-        post.post_content = nil
-        post.valid?
-        expect(post.errors[:post_content]).to include("can't be blank")
-      end
-    end
-
-    context 'when content is present' do
-      it 'is valid' do
-        post.valid?
-        expect(post.errors[:post_content]).to be_blank
-      end
-    end
-
-    context 'when content is > 400 charancters' do
-      it 'is invalid' do
-        post.post_content = 'a' * 401
-        post.valid?
-        expect(post.errors[:post_content].to_s)
-          .to include('maximum is 400')
-      end
-    end
-
-    context 'when content has <= 400 characters' do
-      it 'is valid' do
-        post.post_content = 'a' * 400
-        post.valid?
-        expect(post.errors[:post_content]).to be_blank
-      end
-    end
+    it { is_expected.to validate_presence_of(:post_content) }
+    it { is_expected.to validate_length_of(:post_content).is_at_most(400) }
   end
 
   describe 'Associations' do
-    it 'has many likes' do
-      user2.like(post2)
-      expect(post2.likes.count).to eq 1
-    end
-
-    it 'has many comments' do
-      assc = described_class.reflect_on_association(:comments)
-      expect(assc.macro).to eq :has_many
-    end
-
-    context 'with post subscribers' do
-      it 'has many subscribers' do
-        assc = described_class.reflect_on_association(:subscribers)
-        expect(assc.macro).to eq :has_many
-      end
-
-      it 'update subscribers when a user commented on a post' do
-        user2.comments.create(comment_content: 'new comment', post_id: post2.id)
-        expect(post2.subscribers).to include(user2)
-      end
-    end
-
-    context 'when user likes post' do
-      it 'returns numbers of likes associated with the post' do
-        user2.like(post2)
-        expect(post2.likes_count).to eq 1
-      end
-    end
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to have_many(:comments) }
+    it { is_expected.to have_many(:likes) }
+    it { is_expected.to have_many(:notifications) }
+    it { is_expected.to have_many(:mentions) }
+    it { is_expected.to have_many(:subscribers) }
   end
 end
