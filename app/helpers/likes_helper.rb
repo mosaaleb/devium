9e48c8ffa7a5913d8a72cfa41df6e2ea312829e4
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-05-12 00:55:25 +0200

    change likes button styles

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -3,11 +3,11 @@
 module LikesHelper
   def like_button(likable)
     if current_user.liked?(likable)
-      link_to image_tag('dislike'),
+      link_to fa_icon('heart', class: 'text-danger'),
               polymorphic_path([:dislike, likable], type: likable.class),
               method: :delete, remote: true
     else
-      link_to image_tag('like'),
+      link_to fa_icon('heart', class: 'text-light stroke'),
               polymorphic_path([:like, likable], type: likable.class),
               method: :post, remote: true
     end
